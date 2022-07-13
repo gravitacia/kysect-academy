07eb919295a3abe6eeb9730d11bf2d54f5dc96c0
gravitacia <kocagg@mail.ru>
2022-07-13 22:37:53 +0500

    feat : добавил метод сравнения файлов по хэшу

--- KysectAcademyTask/ComparisonLogic.cs
+++ KysectAcademyTask/ComparisonLogic.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Security.Cryptography;
+using System.Text.Json;
 using NetDiff;
 
 namespace KysectAcademyTask;
@@ -9,7 +10,7 @@ public class ComparisonLogic
     {
         if (path == null) throw new Exception("Your path are empty!");
         string[] allFiles = Directory.GetFiles(path);
-        
+
         int count = 0;
         for (int i = 0; i < allFiles.Length - 1; i++)
         {
@@ -36,5 +37,110 @@ public class ComparisonLogic
             await JsonSerializer.SerializeAsync<ComparisonResult>(fs, comparisonResult);
             Console.WriteLine("Data has been saved to file");
         }
+    }
+
+    static double CompareFilesHash(string? firstPath, string? secondPath)
+    {
+        if (firstPath == null) throw new Exception("Your path are empty!");
+        if (secondPath == null) throw new Exception("Your path are empty!");
+
+        string[] allFirstPathFiles = Directory.GetFiles(firstPath);
+        string[] allSecondPathFiles = Directory.GetFiles(secondPath);
+        
+        int count = 0;
+        double percent = 0;
+        
+        foreach (string curFirstFile in allFirstPathFiles)
+        {
+            foreach (string curSecondFile in allSecondPathFiles)
+            {
+                var first = new FileInfo(curFirstFile);
+                var second = new FileInfo(curSecondFile);
+                
+                byte[] firstHash = MD5.Create().ComputeHash(first.OpenRead());
+                byte[] secondHash = MD5.Create().ComputeHash(second.OpenRead());
+
+                for (int i = 0; i < firstHash.Length; i++)
+                {
+                    if (firstHash[i] != secondHash[i])
+                        count++;
+                }
+                
+                string str1 = File.ReadAllText(curFirstFile);
+                string str2 = File.ReadAllText(curSecondFile);
+                
+                if (str1.Length > str2.Length)
+                {
+                    percent = count / str1.Length;
+                }
+                else
+                {
+                    percent = count / str2.Length;
+                }
+
+                
+            }
+        }
+        return percent;
+    }
+
+
+    public void CompareFolders(string? pathA, string? pathB)
+        {
+            if (pathA != null)
+            {
+                var dir1 = new System.IO.DirectoryInfo(pathA);
+                if (pathB != null)
+                {
+                    var dir2 = new System.IO.DirectoryInfo(pathB);
+
+                
+                    IEnumerable<System.IO.FileInfo> list1 = dir1.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+                    IEnumerable<System.IO.FileInfo> list2 = dir2.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+
+                    
+                    var myFileCompare = new FileCompare();
+
+                    
+                    bool areIdentical = list1.SequenceEqual(list2, myFileCompare);
+                    
+                    if (areIdentical == true)
+                    {
+                        Console.WriteLine("the two folders are the same");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The two folders are not the same");
+                    }
+                
+                    IEnumerable<FileInfo> queryCommonFiles = list1.Intersect(list2, myFileCompare);
+
+                    if (queryCommonFiles.Any())
+                    {
+                        Console.WriteLine("The following files are in both folders:");
+                        foreach (FileInfo v in queryCommonFiles)
+                        {
+                            Console.WriteLine(v.FullName); //shows which items end up in result list  
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("There are no common files in the two folders.");
+                    }
+                
+                    IEnumerable<FileInfo> queryList1Only = (from file in list1
+                        select file).Except(list2, myFileCompare);
+
+                    Console.WriteLine("The following files are in list1 but not list2:");
+                    foreach (FileInfo v in queryList1Only)
+                    {
+                        Console.WriteLine(v.FullName);
+                    }
+                }
+            }
+        
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
     }
 }
--- KysectAcademyTask/FileCompare.cs
+++ KysectAcademyTask/FileCompare.cs
@@ -9,12 +9,7 @@ public class FileCompare : System.Collections.Generic.IEqualityComparer<System.I
         return (x?.Name == y?.Name &&
                 x?.Length == y?.Length);
     }
-
-    // Return a hash that reflects the comparison criteria. According to the
-    // rules for IEqualityComparer<T>, if Equals is true, then the hash codes must  
-    // also be equal. Because equality as defined here is a simple value equality, not  
-    // reference identity, it is possible that two or more objects will produce the same  
-    // hash code.  
+ 
     public int GetHashCode(System.IO.FileInfo obj)
     {
         string s = $"{obj.Name}{obj.Length}";
