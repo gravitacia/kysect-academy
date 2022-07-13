1b36449f6d61bcaeaa35e7ff8a05dd7a42d4f5b4
gravitacia <kocagg@mail.ru>
2022-07-13 21:41:19 +0500

    feat : добавил алгоритм сравнения папок на разницу файлов

---
+++ KysectAcademyTask/FileCompare.cs
@@ -0,0 +1,23 @@
+﻿namespace KysectAcademyTask;
+
+public class FileCompare : System.Collections.Generic.IEqualityComparer<System.IO.FileInfo>
+{
+    public FileCompare() { }
+
+    public bool Equals(FileInfo? x, FileInfo? y)
+    {
+        return (x?.Name == y?.Name &&
+                x?.Length == y?.Length);
+    }
+
+    // Return a hash that reflects the comparison criteria. According to the
+    // rules for IEqualityComparer<T>, if Equals is true, then the hash codes must  
+    // also be equal. Because equality as defined here is a simple value equality, not  
+    // reference identity, it is possible that two or more objects will produce the same  
+    // hash code.  
+    public int GetHashCode(System.IO.FileInfo obj)
+    {
+        string s = $"{obj.Name}{obj.Length}";
+        return s.GetHashCode();
+    }
+}
