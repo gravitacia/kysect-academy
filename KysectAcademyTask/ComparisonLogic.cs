2c4a9ae117c0bbb5aeeb205b865dba9ae89ace74
gravitacia <kocagg@mail.ru>
2022-07-14 12:52:05 +0500

    fix : пофиксил сравнение файлов

--- KysectAcademyTask/ComparisonLogic.cs
+++ KysectAcademyTask/ComparisonLogic.cs
@@ -11,30 +11,35 @@ public class ComparisonLogic
         string[] allFiles = Directory.GetFiles(path);
         
         int count = 0;
+        
         for (int i = 0; i < allFiles.Length - 1; i++)
         {
-            string str1 = File.ReadAllText(allFiles[i]);
-            string str2 = File.ReadAllText(allFiles[i + 1]);
-
-            IEnumerable<DiffResult<char>> results = new Comparator().EntitiesCompare(str1, str2);
-
-            count += results.Count(r => r.Status == NetDiff.DiffStatus.Equal);
-
-            double percent;
-            if (str1.Length > str2.Length)
+            for (int j = i + 1; j < allFiles.Length - 1; j++)
             {
-                percent = count / str1.Length;
-            }
-            else
-            {
-                percent = count / str2.Length;
-            }
+                
+                string str1 = File.ReadAllText(allFiles[i]);
+                string str2 = File.ReadAllText(allFiles[j]);
 
-            var comparisonResult = new ComparisonResult(str1, str2, percent);
-            if (pathToSerialize == null) continue;
-            await using var fs = new FileStream(pathToSerialize, FileMode.OpenOrCreate);
-            await JsonSerializer.SerializeAsync<ComparisonResult>(fs, comparisonResult);
-            Console.WriteLine("Data has been saved to file");
+                IEnumerable<DiffResult<char>> results = new Comparator().EntitiesCompare(str1, str2);
+
+                count += results.Count(r => r.Status == NetDiff.DiffStatus.Equal);
+
+                double percent;
+                if (str1.Length > str2.Length)
+                {
+                    percent = count / str1.Length;
+                }
+                else
+                {
+                    percent = count / str2.Length;
+                }
+
+                var comparisonResult = new ComparisonResult(str1, str2, percent);
+                if (pathToSerialize == null) continue;
+                await using var fs = new FileStream(pathToSerialize, FileMode.OpenOrCreate);
+                await JsonSerializer.SerializeAsync<ComparisonResult>(fs, comparisonResult);
+                Console.WriteLine("Data has been saved to file");
+            }
         }
     }
 }
