00cd06e011cf6d2ff94e59c24f3663bbc889c5be
gravitacia <kocagg@mail.ru>
2022-07-19 03:49:05 +0500

    fix : пофиксил ошибку во время которой падал билд

--- KysectAcademyTask/Config/ConfigFilter.cs
+++ KysectAcademyTask/Config/ConfigFilter.cs
@@ -7,22 +7,23 @@ public class ConfigFilter
     public IEnumerable<FileInfo> GetFinalList(IEnumerable<FileInfo> list)
     {
         return list.Where(f =>
-                (bool)_configuration.FileFilter?.ExtensionsWhiteList!
-                    .Any(x => f.Name.EndsWith(x)))
+                _configuration.FileFilter?.ExtensionsWhiteList != null 
+                && Convert.ToBoolean(_configuration.FileFilter?.ExtensionsWhiteList?
+                    .Any(x => f.Name.EndsWith(x))))
             .Except(list.Where(dir => dir.DirectoryName != null &&
-                                      (bool)_configuration.FileFilter?.ExtensionsBlackList!
-                                          .Any(x => dir.DirectoryName.Contains(x))))
+                                      Convert.ToBoolean(_configuration.FileFilter?.ExtensionsBlackList!
+                                          .Any(x => dir.DirectoryName.Contains(x)))))
             .Intersect(list.Where(author => author.DirectoryName != null 
-                                            && (bool)_configuration.AuthorFilter?.AuthorsWhiteList!
-                                                .Any(x => author.DirectoryName.Contains(x))))
+                                            && Convert.ToBoolean(_configuration.AuthorFilter?.AuthorsWhiteList!
+                                                .Any(x => author.DirectoryName.Contains(x)))))
             .Except(list.Where(author => author.DirectoryName != null 
-                                         && (bool)_configuration.AuthorFilter?.AuthorsBlackList!
-                                             .Any(x => author.DirectoryName.Contains(x))))
+                                         && Convert.ToBoolean(_configuration.AuthorFilter?.AuthorsBlackList!
+                                             .Any(x => author.DirectoryName.Contains(x)))))
             .Intersect(list.Where(dir => dir.DirectoryName != null &&
-                                         (bool)_configuration.SubmissionFilter?.SubmissionDate!
-                                             .Any(x => dir.DirectoryName.Contains(x)))
+                                         Convert.ToBoolean(_configuration.SubmissionFilter?.SubmissionDate!
+                                             .Any(x => dir.DirectoryName.Contains(x))))
                 .Intersect(list.Where(dir => dir.DirectoryName != null &&
-                                             (bool)_configuration.SubmissionFilter?.HomeworkName!
-                                                 .Any(x => dir.DirectoryName.Contains(x)))));
+                                             Convert.ToBoolean(_configuration.SubmissionFilter?.HomeworkName!
+                                                 .Any(x => dir.DirectoryName.Contains(x))))));
     }
 }
