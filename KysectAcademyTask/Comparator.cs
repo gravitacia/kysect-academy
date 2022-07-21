714729a72674dc57506f648a23a5aa4533310971
gravitacia <kocagg@mail.ru>
2022-07-21 05:02:16 +0500

    refactor : переписал логику алгоритма сравнения

--- KysectAcademyTask/Comparator.cs
+++ KysectAcademyTask/Comparator.cs
@@ -19,9 +19,9 @@ public class Comparator
 
         IEnumerable<FileInfo> finalFirstList = new ConfigFilter().GetFinalList(firstList);
         IEnumerable<FileInfo> finalSecondList = new ConfigFilter().GetFinalList(secondList);
-        
-        
-        double result = new ComparisonLogic().CompareFolders(finalFirstList, finalSecondList);
+
+
+        double result = CompareFolders(finalFirstList, finalSecondList);
 
         return result;
     }
@@ -34,8 +34,75 @@ public class Comparator
         IEnumerable<FileInfo> list = dir.GetFiles("*.*", SearchOption.AllDirectories);
         IEnumerable<FileInfo> finalList = new ConfigFilter().GetFinalList(list);
 
-        List<List<double>> result = new ComparisonLogic().CompareFilesInOneFolder(finalList);
-        
+        List<List<double>> result = CompareFilesInOneFolder(finalList);
+
         return result;
+    }
+
+
+    public double CompareFolders(IEnumerable<FileInfo> firstList, IEnumerable<FileInfo> secondList)
+    {
+        var percentsForFile = new List<double>();
+        var percents = new List<List<double>>();
+        var tmpList = new List<double>();
+        var tmpListForPercent = new List<List<double>>();
+        double finalPercent = 0.0;
+        int iterations = 0;
+
+        foreach (FileInfo curFirstFile in firstList)
+        {
+            foreach (FileInfo curSecondFile in secondList)
+            {
+                if (curFirstFile.DirectoryName is null || curSecondFile.DirectoryName is null)
+                    throw new ArgumentNullException();
+                using FileStream first = File.OpenRead(curFirstFile.DirectoryName);
+                using FileStream second = File.OpenRead(curSecondFile.DirectoryName);
+
+                double percent = new ComparisonLogic().CompareFiles(first, second);
+                percentsForFile.Add(percent);
+                tmpList.Add(percent);
+
+
+                iterations++;
+
+                Console.WriteLine($"Comparing {curFirstFile.Name} and {curSecondFile.Name}.");
+                Console.WriteLine($"{iterations} in {secondList.Count() + firstList.Count()}");
+            }
+
+            percents.Add(percentsForFile);
+            tmpListForPercent.Add(tmpList);
+        }
+
+        finalPercent += tmpListForPercent.Sum(curPercent => curPercent.Sum());
+
+        finalPercent = percents.Aggregate(finalPercent, (current, curPercent) => current / curPercent.Sum());
+
+        return finalPercent;
+    }
+
+
+    public List<List<double>> CompareFilesInOneFolder(IEnumerable<FileInfo> list)
+    {
+        var percentsForFile = new List<double>();
+        var percents = new List<List<double>>();
+
+        foreach (FileInfo curFirstFile in list)
+        {
+            foreach (FileInfo curSecondFile in list)
+            {
+                if (curFirstFile.DirectoryName is null || curSecondFile.DirectoryName is null)
+                    throw new ArgumentNullException();
+                using FileStream first = File.OpenRead(curFirstFile.DirectoryName);
+                using FileStream second = File.OpenRead(curSecondFile.DirectoryName);
+
+                double percent = new ComparisonLogic().CompareFiles(first, second);
+                
+                percentsForFile.Add(percent);
+            }
+
+            percents.Add(percentsForFile);
+        }
+
+        return percents;
     }
 }
--- KysectAcademyTask/ComparisonLogic.cs
+++ KysectAcademyTask/ComparisonLogic.cs
@@ -1,62 +1,9 @@
 ﻿
 namespace KysectAcademyTask;
 
-public class ComparisonLogic
+public class ComparisonLogic : IAlgorithmLogic
 {
-    public double CompareFolders(IEnumerable<FileInfo> firstList, IEnumerable<FileInfo> secondList)
-    {
-        var percentsForFile = new List<double>();
-        var percents = new List<List<double>>();
-        var tmpList = new List<double>();
-        var tmpListForPercent = new List<List<double>>();
-        double finalPercent = 0.0;
-        int iterations = 0;
-
-        foreach (FileInfo curFirstFile in firstList)
-        {
-            foreach (FileInfo curSecondFile in secondList)
-            {
-                double percent = CompareFilesByBites(curFirstFile.DirectoryName, curSecondFile.DirectoryName);
-                percentsForFile.Add(percent);
-                tmpList.Add(percent);
-                
-
-                iterations++;
-                
-                Console.WriteLine($"Comparing {curFirstFile.Name} and {curSecondFile.Name}.");
-                Console.WriteLine($"{iterations} in {secondList.Count()+firstList.Count()}");
-            }
-
-            percents.Add(percentsForFile);
-            tmpListForPercent.Add(tmpList);
-        }
-
-        finalPercent += tmpListForPercent.Sum(curPercent => curPercent.Sum());
-
-        finalPercent = percents.Aggregate(finalPercent, (current, curPercent) => current / curPercent.Sum());
-
-        return finalPercent;
-
-    }
-
-    public List<List<double>> CompareFilesInOneFolder(IEnumerable<FileInfo> list)
-    {
-        var percentsForFile = new List<double>();
-        var percents = new List<List<double>>();
-        
-        foreach (FileInfo curFirstFile in list)
-        {
-            percentsForFile.AddRange(list.Select(curSecondFile => 
-                    CompareFilesByBites(curFirstFile.DirectoryName, curSecondFile.DirectoryName))
-                .Where(percent => percent != 1.0));
-
-            percents.Add(percentsForFile);
-        }
-
-        return percents;
-    }
-    
-    private double CompareFilesByBites(string? firstFile, string? secondFile)
+    public double CompareFiles(FileStream? firstFile, FileStream? secondFile)
     {
         int count = 0;
         double percent = 0.0;
@@ -64,25 +11,20 @@ public class ComparisonLogic
         if (firstFile is null) return percent; 
         if (secondFile is null) return percent;
         
-        using FileStream first = File.OpenRead(firstFile);
-        using FileStream second = File.OpenRead(secondFile);
-        for (int i = 0; i < first.Length; i++)
+        for (int i = 0; i < firstFile.Length; i++)
         {
-            if (first.ReadByte() != second.ReadByte())
+            if (firstFile.ReadByte() != secondFile.ReadByte())
                 count++;
         }
 
-        string str1 = File.ReadAllText(firstFile);
-        string str2 = File.ReadAllText(secondFile);
-
-        if (str1.Length > str2.Length)
+        if (firstFile.Length > secondFile.Length)
         {
-            percent = Convert.ToDouble(count) / Convert.ToDouble(str1.Length);
+            percent = Convert.ToDouble(count) / Convert.ToDouble(firstFile.Length);
 
         }
         else
         {
-            percent = Convert.ToDouble(count) / Convert.ToDouble(str2.Length);
+            percent = Convert.ToDouble(count) / Convert.ToDouble(secondFile.Length);
         }
 
         return percent;
