5158a7bbe0f79591393f51e5743dd5df7fbe4569
Aleksander Kosarskii <kocagg@mail.ru>
2022-07-24 21:29:23 +0300

    feat : добавил возможность сохранять результаты сабмитов в базу

--- KysectAcademyTask/DbEntities/SubmissionCompare.cs
+++ KysectAcademyTask/DbEntities/SubmissionCompare.cs
@@ -2,6 +2,14 @@ namespace KysectAcademyTask.DbEntities;
 
 public class SubmissionCompare
 {
+    public SubmissionCompare(int mixedId, string firstPath, string secondPath, double result)
+    {
+    MixedId = mixedId;
+        FirstSubmission = firstPath;
+        SecondSubmission = secondPath;
+        Result = result;
+    }
+    
     public int MixedId { get; set; }
     public string FirstSubmission { get; set; }
     public string SecondSubmission { get; set; }
